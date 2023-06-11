d31da90c69ddfbc4214e0227e5c1a50907d7d46c
Steven <stevenpjohnson@pm.me>
2023-06-11 18:10:02 -0500

    focus

--- js/scripts.js
+++ js/scripts.js
@@ -51,9 +51,9 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
+    // Activate SimpleLightbox plugin for worship items
     new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+        elements: '#worship a.worship-box'
     });
 
 });
